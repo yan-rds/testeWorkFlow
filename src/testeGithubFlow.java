d489fc9b88c02b04299a6e2e56d79a20b7484864
Yan Rodrigues Santos <yyan1207@gmail.com>
2021-09-15 03:30:04 -0300

    Projeto base (entrega média do desafio)

--- src/testeGithubFlow.java
+++ src/testeGithubFlow.java
@@ -29,8 +29,7 @@ public class testeGithubFlow {
         while (repeticaoMenu.equalsIgnoreCase("sim")) {
             System.out.println("Utilize os números indicados para navegar pelo menu");
             System.out.println("1 - Adicionar funcionários");
-            System.out.println("2 - Remover funcionários");
-            System.out.println("3 - Exibir a lista de funcionários");
+            System.out.println("2 - Exibir a lista de funcionários");
             escolhaMenu = leitor.nextInt();
 
             // Estrutura de decisão - Menu
@@ -44,38 +43,22 @@ public class testeGithubFlow {
                         for (int i = 1; i <= qtdeFuncionariosADD; i++) {
                             System.out.println("Por favor insira o CPF do funcionário");
                             cpf = leitor.next();
-                            // Condicional que verificará se o CPF é repetido
-                            if (cadastro.containsKey(cpf)) {
-                                System.out.print("Este CPF já foi cadastrado");
-                            } else {
-                                System.out.println("Nome");
-                                leitor.nextLine();
-                                nome = leitor.nextLine();
-                                System.out.println("Telefone");
-                                telefone = leitor.nextLine();
-                                System.out.println("E-mail");
-                                email = leitor.next();
-                                cadastro.put(cpf, "Nome do funcionário: " + nome + "\n" + "Dados de contato - "
-                                        + "Telefone: " + telefone + " E-mail: " + email);
+                            System.out.println("Nome");
+                            leitor.nextLine();
+                            nome = leitor.nextLine();
+                            System.out.println("Telefone");
+                            telefone = leitor.nextLine();
+                            System.out.println("E-mail");
+                            email = leitor.next();
+                            cadastro.put(cpf, "Nome do funcionário: " + nome + "\n" + "Dados de contato - "
+                                    + "Telefone: " + telefone + " E-mail: " + email);
                             }
                         }
                         System.out.println();
                         System.out.println("Deseja inserir mais algum funcionário? Responda com Sim ou Não");
                         confirmarADD = leitor.next();
-                    }
                     break;
-                case 2: // Sistema para remover funcionários
-                    System.out.println("Digite o CPF do funcionário que você deseja remover");
-                    cpf = leitor.next();
-                    // Condicional pra verificar se esse usuário está cadastrado
-                    if (cadastro.containsKey(cpf)) {
-                        System.out.println("Funcionário removido: \n" + cadastro.get(cpf));
-                        cadastro.remove(cpf);
-                    } else {
-                        System.out.println("Este funcionário não está cadastrado, confira se digitou corretamente");
-                    }
-                    break;
-                case 3: // Exibir a lista de funcionários
+                case 2: // Exibir a lista de funcionários
                     for (String referencia : cadastro.values()) {
                         System.out.println(referencia);
                     }
@@ -83,6 +66,7 @@ public class testeGithubFlow {
                 default: // Número inválido
                     System.out.println("Você digitou uma opção inválida");
             }
+
             System.out.println("Deseja voltar ao menu principal? Responda com Sim ou Não");
             repeticaoMenu = leitor.next();
         }
